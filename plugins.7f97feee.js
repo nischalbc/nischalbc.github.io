0766274c56be5afc72907de3f1cc90bcea6312d4
nisbc <nischalkumar.bc@here.com>
2022-08-16 18:40:16 +0530

    Some more changes

--- plugins.7f97feee.js
+++ plugins.7f97feee.js
@@ -164,7 +164,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65266" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61109" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
